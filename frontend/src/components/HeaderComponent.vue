1165a342faa214d03cda5d7bdc0eff698393d650
vnazarko <88901579+vnazarko@users.noreply.github.com>
2024-10-04 16:35:45 +0300

    перешел на vue

---
+++ frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import {vueTelegramLogin} from 'vue-telegram-login'
+
+function yourCallbackFunction(user) {
+    console.log(user);
+    
+}
+</script>
+<template>
+    <div class="container">
+        <header class="header">
+            <vue-telegram-login 
+                mode="callback"
+                telegram-login="CryptoTrackByNone1qqBot"
+                @callback="yourCallbackFunction" />
+        </header>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
