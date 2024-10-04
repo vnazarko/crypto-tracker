c20fd94308bc43b286e748909a40267141f4abc1
vnazarko <88901579+vnazarko@users.noreply.github.com>
2024-10-04 12:59:55 +0300

    фикс

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -6,7 +6,12 @@ const testCallback = (user: any) => {
 <template>
     <header class="header">
         <h1 class="header_title">CryptoTrack</h1>
-        <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="CryptoTrackByNone1qqBot" data-size="medium" data-onauth="onTelegramAuth(user)" data-request-access="write" @click="testCallback(user)"></script>
+        <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="CryptoTrackByNone1qqBot" data-size="medium" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+        <script type="text/javascript">
+        function onTelegramAuth(user) {
+            console.log(user)
+        }
+        </script>
     </header>
 </template>
 
