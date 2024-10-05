ce17fb451815166b0cf0eb0cecfeaa9cacef3618
vnazarko <88901579+vnazarko@users.noreply.github.com>
2024-10-05 22:58:19 +0300

    добавил CORS

--- api/src/main.py
+++ api/src/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 import uvicorn
 import asyncio
 
+from starlette.middleware.cors import CORSMiddleware
+
 from src.auth.router import router as auth_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+
+origins = [
+    "https://crypto-track.none1qq.ru",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
