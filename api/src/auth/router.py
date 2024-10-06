0af6373699e41e815f0662ac664b457b6a1f2188
vnazarko <88901579+vnazarko@users.noreply.github.com>
2024-10-06 12:25:58 +0300

    добавил хэш к логину

--- api/src/auth/router.py
+++ api/src/auth/router.py
@@ -39,6 +39,7 @@ async def register_user(user: UserSchema):
 
 @router.post('/login', response_model=TokenInfo)
 def auth_user(
+        hash: str,
         user: UserSchema = Depends(validate_auth_user)
 ):
 
