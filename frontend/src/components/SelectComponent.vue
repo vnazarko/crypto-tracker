043ac0008a4a86f2ba018caca3d5f3faedde7ddb
vnazarko <88901579+vnazarko@users.noreply.github.com>
2024-10-10 19:47:20 +0300

    ой

---
+++ frontend/src/components/SelectComponent.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import axios from 'axios';
+
+function getCryptocurrencies() {
+    axios.get(`https://${import.meta.env.VITE_API_URL}/crypto/get`)
+    .then((response) => {
+        return response.data.payload;
+     })
+}
+</script>
+<template>
+    <div class="container">
+        <nav class="select-coin">
+            <label 
+                v-for="(coin, index) in getCryptocurrencies()"
+                :key="index"
+                :for="coin.name" 
+                class="select-coin_label label"
+            >
+                <input 
+                    type="radio" 
+                    name="coin" 
+                    :id="coin.name" 
+                    class="select-coin_item select-item"
+                >
+                {{ coin.name }}
+            </label>
+            
+        </nav>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
